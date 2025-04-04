db6722cc0ffdaaaba2df1a9cf6cbd58979f0e2f9
Richard T. Carback III <rick.carback@gmail.com>
2025-04-04 10:45:34 -0400

    Add licensing to all files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+// Copyright (C) 2024 quip.network
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
 import { ethers } from 'ethers';
 import { QuipWallet__factory, QuipFactory__factory, QuipFactory, QuipWallet } from '../typechain-types';
 import { QUIP_FACTORY_ADDRESS, WOTS_PLUS_ADDRESS } from './addresses';
