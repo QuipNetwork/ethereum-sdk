4219945dbae3eab60fe3ff6f3355d5b78aa08717
Richard T. Carback III <rick.carback@gmail.com>
2025-04-11 11:42:09 -0400

    Add DEPLOYER_ADDRESS

--- src/addresses.ts
+++ src/addresses.ts
@@ -17,6 +17,7 @@
 import { NetworkType, SUPPORTED_NETWORKS } from './index';
 import addresses from './addresses.json';
 
+export const DEPLOYER_ADDRESS = addresses.Deployer;
 export const WOTS_PLUS_ADDRESS = addresses.WOTSPlus;
 export const QUIP_FACTORY_ADDRESS = addresses.QuipFactory;
 
