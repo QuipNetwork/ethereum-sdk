83364e3ee26fe64036ccb51b0b447b88cea3ca66
Richard T. Carback III <rick.carback@gmail.com>
2025-12-17 20:52:23 -0500

    Update scripts for new hardhat tsconfig requirements

--- scripts/getDeploymentBlock.ts
+++ scripts/getDeploymentBlock.ts
@@ -48,13 +48,11 @@ async function main() {
   }
 }
 
-if (require.main === module) {
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 export { main as getDeploymentBlock };
